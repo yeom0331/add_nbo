2fdf9101c584d5e5571aedfdd3d1a708f03c86a8
yeom <yeom0331@naver.com>
2020-04-05 21:55:55 -0700

    Reload main.cpp

--- main.cpp
+++ main.cpp
@@ -3,30 +3,38 @@
 #include <stdlib.h> //for exit
 #include <arpa/inet.h> //for ntohl
 
-void add_nbo() {
-    uint32_t a, b, result;
-    FILE *f1, *f2;
 
-    f1 = fopen("/home/yeom/temp/c/add_nbo/f1.bin", "rb");
-    f2 = fopen("/home/yeom/temp/c/add_nbo/f2.bin", "rb");
-    if(f1 == NULL || f2 == NULL)
-    {
-        printf("error\n");
-        exit(1);
-    }
-
-    fread(&a, 4, 1, f1);
-    fread(&b, 4, 1, f2);
-
-    a = ntohl(a);
-    b = ntohl(b);
-    result = a+b;
-
-    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", a, a, b, b, result, result);
-    fclose(f1);
-    fclose(f2);
+void usage() {
+    printf("syntax : add_nbo <file1> <file2>\n");
+    printf("sample : add_nbo a.bin c.bin\n");
 }
 
-int main() {
-    add_nbo();
+uint32_t add_nbo(char *argv) {
+    uint32_t a;
+    FILE *f;
+
+    f = fopen(argv, "rb");
+    if(f != NULL) {
+        fread(&a, 4, 1, f);
+        a = ntohl(a);
+        return a;
+    }
+    else {
+        printf("Error\n");
+        return -1;
+    }
+    fclose(f);
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 3) {
+        usage();
+        return -1;
+    }
+
+    uint32_t f1, f2, result;
+    f1 = add_nbo(argv[1]);
+    f2 = add_nbo(argv[2]);
+    result = f1+f2;
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", f1, f1, f2, f2, result, result);
 }
